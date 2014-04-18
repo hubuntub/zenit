e361827ef357087aeca42f4290039c0334c86f94
Vincent Demeester <vincent+git@demeester.fr>
2014-04-18 19:44:34 +0200

    Add gitignore for idea files

--- src/main/java/com/github/hubuntub/zenit/repository/UserRepository.java
+++ src/main/java/com/github/hubuntub/zenit/repository/UserRepository.java
@@ -8,7 +8,6 @@ import com.github.hubuntub.zenit.model.User;
 
 @Repository("userRepository")
 public class UserRepository extends AbstractRepository<User> {
-
 	
 	// private static final String QUERY_GET_USER = "SELECT u FROM USER u ORDER BY u.username";
 	private static final String QUERY_GET_USER = "from User";
--- src/main/java/com/github/hubuntub/zenit/web/controller/UserController.java
+++ src/main/java/com/github/hubuntub/zenit/web/controller/UserController.java
@@ -18,7 +18,6 @@ import com.github.hubuntub.zenit.service.UserService;
 @Controller
 public class UserController{
 
-
 	@Autowired
 	private UserService userService;
 	
